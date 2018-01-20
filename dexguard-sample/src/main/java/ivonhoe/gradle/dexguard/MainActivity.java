832c18f03b7abbaec469dcdc7ccb86971ca7889a
stytooldex <2284467793@qq.com>
2018-01-20 21:21:06 +0800

    Update MainActivity.java

--- dexguard-sample/src/main/java/ivonhoe/gradle/dexguard/MainActivity.java
+++ dexguard-sample/src/main/java/ivonhoe/gradle/dexguard/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+ivonhoe.gradle.dexguard.MainActivity:onCreate
+ivonhoe.gradle.dexguard.MainActivity$HelloWorld:testDexGuard
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
